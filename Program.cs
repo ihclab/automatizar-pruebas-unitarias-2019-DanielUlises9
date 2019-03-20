f8aef20cb0c0d7c67fd99633ec0c1b7b02fd7090
DanielUlises9 <dmendoza7@ucol.mx>
2019-03-20 10:04:02 -0600

    Problema 02

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace AutomatizarPruebasUnitarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lineas = File.ReadAllLines("CasosPrueba.txt");
+            string[,] casos = new string[lineas.Length, 4];
+            for (int i = 0; i < lineas.Length; i++)
+            {
+                string[] linea = lineas[i].Split(':');
+                for (int j = 0; j < linea.Length; j++)
+                {
+                    casos[i,j] = linea[j];
+                    System.Console.WriteLine(casos[i,j]);
+                }
+            }
+        }
+    }
+}
